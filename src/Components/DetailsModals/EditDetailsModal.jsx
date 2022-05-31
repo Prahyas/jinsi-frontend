b6f8e6562d41e14358ff2a68fc345fca058e3701
prahyas <xrestha17@gmail.com>
2022-05-31 22:49:30 +0545

    added new collection for testing

--- src/Components/DetailsModals/EditDetailsModal.jsx
+++ src/Components/DetailsModals/EditDetailsModal.jsx
@@ -302,7 +302,7 @@ const EditDetailsModal = ({
                         onChange={(event) => handleInputChange(index, event)}
                       />
                       <input
-                        type='text'
+                        type='number'
                         class='w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500'
                         required
                         name='quantity'
